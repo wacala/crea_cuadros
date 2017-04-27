7979ac7188d1616006490627ab6b809b9d53959e
wacala <walter.rojas@me.com>
2017-04-26 19:35:39 -0500

    Commit

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
+
+# class CuadroComparativo(models.Model):
+# 	autor = models.ForeignKey('auth.User')
+# 	fecha_ejecucion = models.DateTimeField(default=timezone.now)
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 
 # Create your views here.
+def app(request):
+    return render(request, 'app/app.html', {})
